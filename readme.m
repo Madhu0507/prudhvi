777fae788195574269b268d7e93cb89cc9cdfdc6
Sample_Project <anilkumar4274@gmail.com>
2019-08-26 19:08:25 +0530

    Create a readme file

---
+++ readme.m
@@ -0,0 +1,6 @@
+echo "# artificial9intelligence" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/anilkumar4274/artificial9intelligence.git
+git push -u origin master
